1c24dfd777390eddae89d5bf7028b7a03c8a2be4
Danny Berger <dberger@pivotal.io>
2016-02-10 08:21:52 -0800

    Retry aws s3 cp a few times if it fails

--- ci/pipelines/stemcell/tasks/publish.sh
+++ ci/pipelines/stemcell/tasks/publish.sh
@@ -8,8 +8,14 @@ export VERSION=$( cat version/number | sed 's/\.0$//;s/\.0$//' )
 for file in $COPY_KEYS ; do
   file="${file/\%s/$VERSION}"
 
-  echo "Publishing $file..."
-  aws s3 cp "s3://$BUCKET_NAME/$CANDIDATE_PREFIX$file" "s3://$BUCKET_NAME/$PUBLISHED_PREFIX$file"
+  echo "$file"
+
+  # occasionally this fails for unexpected reasons; retry a few times
+  for i in {1..4}; do
+    aws s3 cp "s3://$BUCKET_NAME/$CANDIDATE_PREFIX$file" "s3://$BUCKET_NAME/$PUBLISHED_PREFIX$file" \
+      && break \
+      || sleep 5
+  done
 
   echo ""
 done
